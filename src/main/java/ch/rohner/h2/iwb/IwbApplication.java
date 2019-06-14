24bfdbd824e858e182d10b8b09672002d56db5c9
Aubacca <f1efed325d6b84e4a1b293d3877e155f>
2019-06-14 10:53:28 +0200

    Initial upload

---
+++ src/main/java/ch/rohner/h2/iwb/IwbApplication.java
@@ -0,0 +1,13 @@
+package ch.rohner.h2.iwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwbApplication.class, args);
+    }
+
+}
